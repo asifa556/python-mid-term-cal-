7c2a55a19605406381c359dd91014d2efd575d7e
asifa556 <156574922+asifa556@users.noreply.github.com>
2024-01-14 14:48:14 +0500

    Add files via upload

---
+++ asifa.py
@@ -0,0 +1,23 @@
+print("calculator")
+num1=int(input("enter the value 1:"))
+num2=int(input("enter the value 2:"))
+print("press + for addition /n press - for subtraction /n press * for multiplication /n /for division")
+choice=input("enter the choice for + - * /")
+if choice == "+":
+    print(num1+num2)
+elif choice == "-":
+    print(num1-num2)
+elif choice == "*":
+    print(num1*num2)
+elif choice == "/":
+    print (num1/num2)
+else:
+    print("invalid number")
+                  
+           
+                
+
+
+
+        
+         
